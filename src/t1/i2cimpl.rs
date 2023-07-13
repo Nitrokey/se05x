3932f59cec4eca71bffd55d5b01a9bcf5064bd62
Sosthène Guédon <sosthene@nitrokey.com>
2023-07-13 14:20:21 +0200

    Add lpc55 impl

--- src/t1/i2cimpl.rs
+++ src/t1/i2cimpl.rs
@@ -13,3 +13,19 @@ mod nrf52832 {
         }
     }
 }
+
+#[cfg(feature = "lpc55")]
+mod lpc55 {
+    use crate::t1::I2CErrorNack;
+
+    use lpc55_hal::drivers::i2c::Error;
+
+    impl I2CErrorNack for Error {
+        fn is_address_nack(&self) -> bool {
+            matches!(self, Error::NackAddress)
+        }
+        fn is_data_nack(&self) -> bool {
+            matches!(self, Error::NackData)
+        }
+    }
+}
